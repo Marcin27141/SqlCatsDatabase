b52ffd054ae7f31eee702190d62f8c46f955c003
Marcin <marcin.tkocz@op.pl>
2023-11-11 22:14:54 +0100

    fixed list3 task 36, solved 37 and 38

--- lista3.sql
+++ lista3.sql
@@ -48,7 +48,6 @@ DECLARE
     liczba_modyfikacji NUMBER(2) := 0;
 BEGIN
     SELECT SUM(NVL(przydzial_myszy,0)) INTO suma_przydzialow FROM Kocury;
-    DBMS_OUTPUT.PUT(suma_przydzialow);
     <<przydzialy>>LOOP
         FOR kocur IN kocury LOOP
             EXIT przydzialy WHEN suma_przydzialow > 1050;
@@ -57,14 +56,17 @@ BEGIN
             IF (kocur.pm + dodatkowy_przydzial > kocur.maxMyszy)
                 THEN dodatkowy_przydzial := (kocur.maxMyszy - kocur.pm);
             END IF;
+
+            IF (dodatkowy_przydzial > 0) THEN
+                liczba_modyfikacji := liczba_modyfikacji + 1;
+            END IF;
+            
             
             UPDATE Kocury
             SET przydzial_myszy = NVL(przydzial_myszy,0) + dodatkowy_przydzial
             WHERE CURRENT OF kocury;
             
             suma_przydzialow := suma_przydzialow + dodatkowy_przydzial;
-            liczba_modyfikacji := liczba_modyfikacji + 1;
-            DBMS_OUTPUT.PUT(suma_przydzialow);
         END LOOP;
     END LOOP;
     DBMS_OUTPUT.PUT('Calk. przydzial w stadku ');
@@ -72,6 +74,61 @@ BEGIN
     DBMS_OUTPUT.PUT(' Zmian - ');
     DBMS_OUTPUT.PUT_LINE(liczba_modyfikacji);
 END;
-
-SELECT imie, przydzial_myszy "Myszki po podwyzce" FROM KOCURY
 ROLLBACK;
+
+//zadanie 37
+DECLARE
+    CURSOR ranking IS
+    SELECT pseudo, NVL(przydzial_myszy, 0) + NVL(myszy_extra, 0) sumaMyszy
+    FROM Kocury
+    ORDER BY 2 DESC;
+    kocur kocury%ROWTYPE;
+    i NUMBER(1) := 1;
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('Nr  Pseudonim    Zjada');
+    DBMS_OUTPUT.PUT_LINE('----------------------');
+    FOR kocur IN ranking LOOP
+        EXIT WHEN i > 5;
+        DBMS_OUTPUT.PUT_LINE(i||'   '||RPAD(kocur.pseudo, 8, ' ')||'     '||kocur.sumaMyszy);
+        i := i + 1;
+    END LOOP;
+END;
+
+
+//zadanie 38
+DECLARE
+    CURSOR kocury IS
+    SELECT pseudo, imie, szef
+    FROM Kocury
+    WHERE funkcja IN ('KOT', 'MILUSIA');
+    kocur kocury%ROWTYPE;
+    szef_i kocury%ROWTYPE;
+    liczba_przelozonych NUMBER(2) := &liczba_przelozonych;
+BEGIN
+    DBMS_OUTPUT.PUT(RPAD('Imie', 13, ' '));
+    FOR i IN 1..liczba_przelozonych LOOP
+        DBMS_OUTPUT.PUT('  |  '||'Szef '||RPAD(i, 7, ' '));
+    END LOOP;
+    DBMS_OUTPUT.NEW_LINE();
+    DBMS_OUTPUT.PUT('-------------');
+    FOR i IN 1..liczba_przelozonych LOOP
+        DBMS_OUTPUT.PUT(' --- '||'------------');
+    END LOOP;
+    DBMS_OUTPUT.NEW_LINE();
+        
+    FOR kocur IN kocury LOOP
+        DBMS_OUTPUT.PUT(RPAD(kocur.imie, 13, ' '));
+        FOR i IN 1..liczba_przelozonych LOOP
+            IF kocur.szef IS NULL THEN
+                DBMS_OUTPUT.PUT('  |  '||RPAD(' ', 12, ' '));
+            ELSE
+                SELECT pseudo, imie, szef INTO szef_i
+                FROM Kocury
+                WHERE pseudo = kocur.szef;
+                DBMS_OUTPUT.PUT('  |  '||RPAD(szef_i.imie, 12, ' '));
+                kocur := szef_i;
+            END IF;
+        END LOOP;
+        DBMS_OUTPUT.NEW_LINE();
+    END LOOP;
+END;
